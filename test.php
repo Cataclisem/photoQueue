773e9e444c964d2a03aa192b926ed3897e9963ab
Christopher Kristiansen <christopher@kristiansenz.com>
2026-02-17 00:17:22 +0100

    Removed original files and started from scratch

---
+++ test.php
@@ -0,0 +1,30 @@
+
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "test";
+    $conn = NULL;
+
+    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+
+	//include("database.php");
+
+
+	function insertSQL($conn, $IDyearClass, $class, $gradYear, $time) {
+		$sqlQuerry = "INSERT INTO queue (IDyearClass, class, gradYear, time) VALUES ('$IDyearClass', '$class', $gradYear, '$time')";	
+		try{
+			mysqli_query($conn, $sqlQuerry);
+			echo "Class added";
+		}
+		catch(mysqli_sql_exception)
+		{
+			echo 'Something Wrong';
+		}
+	}
+
+	insertSQL($conn, "2018y", "3.y", 2021, "18:30:00");
+
+
+	mysqli_close($conn);
+?>
