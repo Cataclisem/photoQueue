66c5f21bf7b5c7fb99d922d602695de366d0d3c6
J Christopher Lentfer Kristiansen <202005794@uni.au.dk>
2026-02-17 15:46:01 +0100

    Tried to add a button

--- test.php
+++ test.php
@@ -1,30 +1,27 @@
-
 <?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "test";
-    $conn = NULL;
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "test";
+$conn = NULL;
 
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
-	//include("database.php");
+//include("database.php");
 
 
-	function insertSQL($conn, $IDyearClass, $class, $gradYear, $time) {
-		$sqlQuerry = "INSERT INTO queue (IDyearClass, class, gradYear, time) VALUES ('$IDyearClass', '$class', $gradYear, '$time')";	
-		try{
-			mysqli_query($conn, $sqlQuerry);
-			echo "Class added";
-		}
-		catch(mysqli_sql_exception)
-		{
-			echo 'Something Wrong';
-		}
+function insertSQL($conn, $IDyearClass, $class, $gradYear, $time)
+{
+	$sqlQuerry = "INSERT INTO queue (IDyearClass, class, gradYear, time) VALUES ('$IDyearClass', '$class', $gradYear, '$time')";
+	try {
+		mysqli_query($conn, $sqlQuerry);
+		echo "Class added";
+	} catch (mysqli_sql_exception) {
+		echo 'Something Wrong';
 	}
+}
 
-	insertSQL($conn, "2018y", "3.y", 2021, "18:30:00");
+insertSQL($conn, "2018y", "3.y", 2021, "18:30:00");
 
 
-	mysqli_close($conn);
-?>
+mysqli_close($conn);
