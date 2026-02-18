b944c9cf29270f1f9a257268090a5649ab308cd1
Christopher Kristiansen <christopher@kristiansenz.com>
2026-02-18 21:05:45 +0100

    Added validation of input and comments

--- Testing/test.php
+++ Testing/test.php
@@ -6,27 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-    <body>
-
-        <form id="myForm" action= "test.php" method = "post">
-            <input type="submit" name="submit"> <br>
-            <label for="IDYearclass0">ID:</label> <label for="class0">Class:</label> <label for="gradYear0">Year:</label> <label for="time0">Time:</label> <br>
-            <input id="IDYearclass0" type="text" name="IDclassYear[0]" placeholder="ID 1" pattern="[0-9]{4}[a-z]{1}" title="Format: 2019x"> 
-            <input id="class0" type="text" name="class[0]" placeholder="Class 1" pattern="[1-3]\.[a-z]{1}" title="Format: 3.x"> 
-            <input id="gradYear0" type="text" name="gradYear[0]" placeholder="Year 1" pattern="[0-9]{4}" title="Format: 0123">
-            <input id="time0" type="text" name="time[0]" placeholder="Time 1" pattern="[0-9]{2}:[0-9]{2}" title="Format: 12:34"> <br>
+   <body>
+        <form id="myForm"  method="post" > <!--Change the action to the page you want to submit the form to. action="https://www.example.com/submit" <input type="hidden" name="action" value="post_to_DB">-->
+             <br> 
+            <input id="class0" type="text" name="class[0]" placeholder="Klasse nr. 1" pattern="[1-3]\.[a-z]{1,2}" title="Format: 3.x, 2.a, 3.ux,..."> 
+            <input id="gradYear0" type="text" name="gradYear[0]" placeholder="Dimittend år nr. 1" pattern="[0-9]{4}" title="Format: 2019, 1988, 2006...">
+            <input id="time0" type="text" name="time[0]" placeholder="Tidspunkt nr. 1" pattern="[0-2]{1}[0-9]{1}:[0-6]{1}[0-9]{1}" title="Format: 12:34, 14:40, 09:36,..."> <br>
+            <input type="submit" name="submit">
         </form> 
         <button onclick="addOption();">Add Class</button> 
 
         <script> 
             var optionNumber = 1; //The first option to be added is number 1 
-            const idList = ["IDclassYear", "class", "gradYear", "time"]; //List of the first part of the id
-            const placeholderList = ["ID", "Class", "Year", "Time"];   //List of the first part of the placeholder
+            const idList = ["class", "gradYear", "time"]; //List of the first part of the id
+            const placeholderList = ["Klasse nr.", "Dimittend år nr.", "Tidspunkt nr."];   //List of the first part of the placeholder
 
             function addOption() { 
                 var theForm = document.getElementById("myForm"); //Get the form element
 
-                for (let i= 0; i < 4; i++) {
+                for (let i= 0; i < 3; i++) {
 
                     var newOption = document.createElement("input"); //Create a new input element
                     newOption.id = idList[i] + optionNumber; 
@@ -57,42 +55,111 @@ $conn = NULL;
 
 $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
 
+/**
+ * Insert a class into the database. By using an SQL querry, the class, graduation  year and time are inserted into the database
+ * @param object $conn The database connection.
+ * @param string $class The class to insert.
+ * @param string $gradYear The graduation year to insert.
+ * @param string $time The time to insert.  
+ * @return void
+ */
 function insertSQL($conn, $class, $gradYear, $time)
 {
 	$sqlQuerry = "INSERT INTO queue (class, gradYear, time) VALUES ('$class', $gradYear, '$time')";
 	try {
 		mysqli_query($conn, $sqlQuerry);
 		echo "Class added";
-	} catch (mysqli_sql_exception) {
-		echo 'Something Wrong';
+	} catch (mysqli_sql_exception $e) {
+		echo 'Database error: ' . mysqli_error($conn);
 	}
 }
 
+/**
+ *  Change the time format to HH:MM:SS.
+ *  @param string $time The time to change.
+ *  @return string The changed time.
+ */
 function changeTimeFormat($time) {
 	$time = "{$time}:00";
 	return $time;
 }
 
-function sendToDB($conn, $POST) {
-	for ($i = 0; $i < count($POST["class"]); $i++) {
-		$class = $POST["class"][$i];
-		$gradYear = $POST["gradYear"][$i];
-		$time = changeTimeFormat($POST["time"][$i]);
-		insertSQL($conn, $class, $gradYear, $time);
-	}
+
+/**
+ *  Filter the time to ensure it is valid. By using "FILTER_VALIDATE_REGEXP" to check that it is in the format of HH:MM and that the hours are between 00 and 23 and the minutes are between 00 and 59.
+ *  @param string $time The time to filter.
+ *  @return string The filtered time.
+ */
+function filterTime($time) {
+    $time = filter_var($time, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[0-2][0-9]:[0-5][0-9]$/")));
+    return $time;
 }
 
 
+/**
+ *  Filter the graduation year to ensure it is valid. By validating that it is an integer and has 4 digits.
+ *  @param string $gradYear The graduation year to filter.
+ *  @return string The filtered graduation year.
+ */
+function filterGradYear($gradYear) {
+    if (!(filter_var($gradYear, FILTER_VALIDATE_INT) and count(str_split($gradYear)) == 4)) {
+        throw new Exception("{$gradYear} is an Invalid grad year");
+    }else {
+    return $gradYear;
+    }
+}
+
+
+/**
+ *  Filter the class name to ensure it is valid.
+ *  @param string $class The class name to filter.
+ *  @return string The filtered class name.
+ */
+function filterClass($class) {
+    if (!filter_var($class, FILTER_SANITIZE_SPECIAL_CHARS)) {
+        throw new Exception("{$class} is an Invalid class");
+    } else {
+    return $class;
+    }
+}
+
+/**
+ *  Send the POST data to the database.
+ *  @param object $conn The database connection.
+ *  @param array $POST The POST data.
+ *  @return void
+ */
+function sendToDB($conn, $POST) {
+	for ($i = 0; $i < count($POST["class"]); $i++) {
+        try {
+            $class = filterClass($POST["class"][$i]);
+            $gradYear = filterGradYear($POST["gradYear"][$i]);
+            $time = changeTimeFormat(filterTime($POST["time"][$i]));
+            insertSQL($conn, $class, $gradYear, $time);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+	}
+}
+
+/**
+ *  Get data from the database and display it.
+ *  @param object $conn The database connection.
+ *  @return void
+ */
 function getFromDB($conn) {
     $sqlQuerry = "SELECT * FROM queue";
     $result = mysqli_query($conn, $sqlQuerry);
     foreach ($result as $row) {
-        $time = substr($row->time, 0, -3);
-		echo "<h4 style='display:inline'>{$row->class} </h4> <h4 style='display:inline'>{$row->gradYear} </h4> <h4 style='display:inline'>{$time} </h4> <br>";
+        $time = substr($row["time"], 0, -3);
+		echo "<h4 style='display:inline'>{$row['class']} </h4> <h4 style='display:inline'>{$row['gradYear']} </h4> <h4 style='display:inline'>{$time} </h4> <br>";
 		echo "<hr style='width:80%;text-align:centered;margin-left:10%'>";
     }
 }
 
+getFromDB($conn);
+
+sendToDB($conn, $_POST);
 
 mysqli_close($conn);
 ?>
--- WordPress/admin-post.php
+++ WordPress/admin-post.php
@@ -31,28 +31,81 @@ do_action( 'admin_init' );
  * 
  */
 
+
+/**
+ *  Change the time format to HH:MM:SS.
+ *  @param string $time The time to change.
+ *  @return string The changed time.
+ */
 function changeTimeFormat($time) {
 	$time = "{$time}:00";
 	return $time;
 }
 
-function addToDB(){
-	global $wpdb;
-	for ($i = 0; $i < count($_POST["class"]); $i++) {
-		$class = $_POST["class"][$i];
-		$gradYear = $_POST["gradYear"][$i];
-		$time = changeTimeFormat($_POST["time"][$i]);
-		$data = array("class"=>$class, "gradYear"=>$gradYear, "time"=>$time);
-		
-		if (!$wpdb->insert("Queue", $data)) {
-			print_r($wpdb->last_error);
-			echo "<br> Something went wrong when intserting the class {$gradYear} {$class} with time {$time} data. Check if the data is correct or is a duplication and try again.";
-		}
-	}
-	wp_safe_redirect(home_url()); //Change this to the page you want to redirect to after the form is submitted
-	exit();
+
+/**
+ *  Filter the time to ensure it is valid. By using "FILTER_VALIDATE_REGEXP" to check that it is in the format of HH:MM and that the hours are between 00 and 23 and the minutes are between 00 and 59.
+ *  @param string $time The time to filter.
+ *  @return string The filtered time.
+ */
+function filterTime($time) {
+    $time = filter_var($time, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[0-2][0-9]:[0-5][0-9]$/")));
+    return $time;
 }
 
+
+/**
+ *  Filter the graduation year to ensure it is valid. By validating that it is an integer and has 4 digits.
+ *  @param string $gradYear The graduation year to filter.
+ *  @return string The filtered graduation year.
+ */
+function filterGradYear($gradYear) {
+    if (!(filter_var($gradYear, FILTER_VALIDATE_INT) and count(str_split($gradYear)) == 4)) {
+        throw new Exception("{$gradYear} is an Invalid grad year");
+    }else {
+    return $gradYear;
+    }
+}
+
+
+/**
+ *  Filter the class name to ensure it is valid.
+ *  @param string $class The class name to filter.
+ *  @return string The filtered class name.
+ */
+function filterClass($class) {
+    if (!filter_var($class, FILTER_SANITIZE_SPECIAL_CHARS)) {
+        throw new Exception("{$class} is an Invalid class");
+    } else {
+    return $class;
+    }
+}
+
+/**
+ *  Add the submitted classes to the database. Through an add_action hook, this function is called when the form is submitted. <br>
+ *  It goes through each class that was submitted in the form and adds it to the database. After the form is submitted, it will redirect to page of your choosing.
+ *  @return void
+ */
+function addToDB(){
+	global $wpdb; //This is the global variable that allows us to interact with the database in WordPress
+	for ($i = 0; $i < count($_POST["class"]); $i++) { //This loop goes through each class that was submitted in the form and adds it to the database
+		try {
+        	$class = filterClass($_POST["class"][$i]);
+        	$gradYear = filterGradYear($_POST["gradYear"][$i]);
+        	$time = changeTimeFormat(filterTime($_POST["time"][$i]));
+			$data = array("class"=>$class, "gradYear"=>$gradYear, "time"=>$time);
+			if (!$wpdb->insert("Queue", $data)) {
+				print_r($wpdb->last_error);
+				echo "<br> Something went wrong when intserting the class {$gradYear} {$class} with time {$time} data. Check if the data is correct or is a duplication and try again.";
+			}
+			wp_safe_redirect(home_url()); //Change this to the page you want to redirect to after the form is submitted
+			exit();
+
+		} catch (Exception $e) {
+		echo $e->getMessage();
+		}
+	}
+}
 
 //add_action( 'admin_post_post_to_DB', 'testfunc3' );
 add_action( 'admin_post_nopriv_post_to_DB', 'addToDB' );
